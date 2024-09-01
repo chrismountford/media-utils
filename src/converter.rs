35a7b0cc2b6b4689e7c820e55ef5ba7e44559201
Chris <chris.mountford.92@googlemail.com>
2024-09-01 14:07:14 +0100

    adding function to extract name

--- src/converter.rs
+++ src/converter.rs
@@ -1,17 +1,30 @@
 use std::process::Command;
 use std::fs;
 
-pub fn convert(file_path: String) -> Vec<u8> {
-    let args_arr = ["-i", &file_path, "-c:v", "libx264", "-b:v", "2M", "-c:a", "aac", "BigBuckBunny.mov"];
+use regex::Regex;
+
+pub fn convert(file_path: &String, output_file: &String) -> Vec<u8> {
+    let args_arr = ["-i", &file_path, "-c:v", "libx264", "-b:v", "2M", "-c:a", "aac", output_file];
 
     let output = Command::new("ffmpeg").args(args_arr).output().expect("failed to run ffmpeg command");
-    // let output = Command::new("ffmpeg").output().expect("failed");
     output.stdout
+}
+
+pub fn extract_film_name(file_name: &String) -> String {
+    let re = Regex::new(r"(.*)_t\d+\..*").unwrap();
+
+    let Some(output) = re.captures(&file_name) else {
+        println!("No match");
+        return String::from("");
+    };
+
+    return (&output[1]).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use std::path::Path;
+    use test_case::test_case;
     use super::*;
     
     #[test]
@@ -19,11 +32,22 @@ mod tests {
         let input_file_path = String::from("big_buck_bunny_720p_2mb.mp4");
         let expected_file = String::from("BigBuckBunny.mov");
 
-        convert(input_file_path);
+        convert(&input_file_path, &expected_file);
         
         let converted_file_exists = Path::new(&expected_file).exists();
         assert!(converted_file_exists);
 
         fs::remove_file(expected_file).expect("Error when cleaning up after test");
+    }
+
+    #[test_case("my_film_t01.mkv", "my_film")]
+    #[test_case("this_is_another_t09.mkv", "this_is_another")]
+    fn should_extract_the_film_name(input: &str, expected: &str) {
+        let input = String::from(input);
+
+        let actual = extract_film_name(&input);
+        let expected = String::from(expected);
+
+        assert_eq!(actual, expected);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use converter::extract_film_name;
+
 mod converter;
 
 fn main() {
-    let result = converter::convert(String::from("big_buck_bunny_720p_2mb.mp4"));
-    println!("{result:?}");
+    let file = String::from("big_buck_bunny_720p_2mb.mp4");
+    let output_name = extract_film_name(&file);
+
+    converter::convert(&file, &output_name);
+    
+    println!("Complete!");
 }
